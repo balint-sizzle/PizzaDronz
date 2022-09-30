afe013bbb79609739587218d4325628bec06abb8
balint-sizzle <s2064308@ed.ac.uk>
2022-09-30 20:53:15 +0100

    inCentralArea functionality complete

---
+++ src/main/java/uk/ac/ed/inf/GeometryUtil.java
@@ -0,0 +1,147 @@
+package uk.ac.ed.inf;
+
+// A Java program to check if a given point
+// lies inside a given polygon
+// Refer https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+// for explanation of functions onSegment(),
+// orientation() and doIntersect()
+class GeometryUtil {
+
+    // Define Infinite (Using INT_MAX
+    // caused overflow problems)
+    static int INF = 10000;
+
+    static class Point {
+        double x;
+        double y;
+
+        public Point(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    ;
+
+    // Given three collinear points p, q, r,
+    // the function checks if point q lies
+    // on line segment 'pr'
+    static boolean onSegment(Point p, Point q, Point r) {
+        if (q.x <= Math.max(p.x, r.x) &&
+                q.x >= Math.min(p.x, r.x) &&
+                q.y <= Math.max(p.y, r.y) &&
+                q.y >= Math.min(p.y, r.y)) {
+            return true;
+        }
+        return false;
+    }
+
+    // To find orientation of ordered triplet (p, q, r).
+    // The function returns following values
+    // 0 --> p, q and r are collinear
+    // 1 --> Clockwise
+    // 2 --> Counterclockwise
+    static int orientation(Point p, Point q, Point r) {
+        double val = (q.y - p.y) * (r.x - q.x)
+                - (q.x - p.x) * (r.y - q.y);
+
+        if (val == 0) {
+            return 0; // collinear
+        }
+        return (val > 0) ? 1 : 2; // clock or counterclock wise
+    }
+
+    // The function that returns true if
+    // line segment 'p1q1' and 'p2q2' intersect.
+    static boolean doIntersect(Point p1, Point q1,
+                               Point p2, Point q2) {
+        // Find the four orientations needed for
+        // general and special cases
+        int o1 = orientation(p1, q1, p2);
+        int o2 = orientation(p1, q1, q2);
+        int o3 = orientation(p2, q2, p1);
+        int o4 = orientation(p2, q2, q1);
+
+        // General case
+        if (o1 != o2 && o3 != o4) {
+            return true;
+        }
+
+        // Special Cases
+        // p1, q1 and p2 are collinear and
+        // p2 lies on segment p1q1
+        if (o1 == 0 && onSegment(p1, p2, q1)) {
+            return true;
+        }
+
+        // p1, q1 and p2 are collinear and
+        // q2 lies on segment p1q1
+        if (o2 == 0 && onSegment(p1, q2, q1)) {
+            return true;
+        }
+
+        // p2, q2 and p1 are collinear and
+        // p1 lies on segment p2q2
+        if (o3 == 0 && onSegment(p2, p1, q2)) {
+            return true;
+        }
+
+        // p2, q2 and q1 are collinear and
+        // q1 lies on segment p2q2
+        if (o4 == 0 && onSegment(p2, q1, q2)) {
+            return true;
+        }
+
+        // Doesn't fall in any of the above cases
+        return false;
+    }
+
+    // Returns true if the point p lies
+    // inside the polygon[] with n vertices
+    static boolean isInside(Point polygon[], int n, Point p) {
+        // There must be at least 3 vertices in polygon[]
+        if (n < 3) {
+            return false;
+        }
+
+        // Create a point for line segment from p to infinite
+        Point extreme = new Point(INF, p.y);
+
+        // To count number of points in polygon
+        // whose y-coordinate is equal to
+        // y-coordinate of the point
+        int decrease = 0;
+
+        // Count intersections of the above line
+        // with sides of polygon
+        int count = 0, i = 0;
+        do {
+            int next = (i + 1) % n;
+
+            if (polygon[i].y == p.y) decrease++;
+
+            // Check if the line segment from 'p' to
+            // 'extreme' intersects with the line
+            // segment from 'polygon[i]' to 'polygon[next]'
+            if (doIntersect(polygon[i], polygon[next], p, extreme)) {
+                // If the point 'p' is collinear with line
+                // segment 'i-next', then check if it lies
+                // on segment. If it lies, return true, otherwise false
+                if (orientation(polygon[i], p, polygon[next]) == 0) {
+                    return onSegment(polygon[i], p,
+                            polygon[next]);
+                }
+
+                count++;
+            }
+            i = next;
+        } while (i != 0);
+
+        // Reduce the count by decrease amount
+        // as these points would have been added twice
+        count -= decrease;
+
+        // Return true if count is odd, false otherwise
+        return (count % 2 == 1); // Same as (count%2 == 1)
+    }
+}
--- src/main/java/uk/ac/ed/inf/LngLat.java
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -9,8 +9,13 @@ public class LngLat {
     public boolean inCentralArea(){
         CentralClient client = new  CentralClient();
         ArrayList<double[]> markers = client.getCentralMarkers();
+        ArrayList<GeometryUtil.Point> centralPolygon = new ArrayList<>();
+        GeometryUtil.Point p = new GeometryUtil.Point(lng,lat);
+        for (int i=0; i<markers.size(); i++){
+            centralPolygon.add(new GeometryUtil.Point(markers.get(i)[0], markers.get(i)[1]));
+        }
 
-        return false;
+        return GeometryUtil.isInside(centralPolygon.toArray(new GeometryUtil.Point[markers.size()]), markers.size(), p);
     }
 
     public double distanceTo(LngLat c){
