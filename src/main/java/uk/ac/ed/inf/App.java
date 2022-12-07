6bcc570f1ca4e3d77f20127f9d4a08c7ff2efaff
Balint Szilagyi <s2064308@ed.ac.uk>
2022-12-07 11:38:00 +0000

    c2c3-beta
    jar executable working

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -1,9 +1,7 @@
 package uk.ac.ed.inf;
 
 public class App {
-    public static void main(String args[]){
-        LngLat zero = new LngLat(0,0);
-        System.out.println(zero.nextPosition(Direction.East).lng);
-        System.out.println(zero.nextPosition(Direction.West).lng);
+    public static void main(String args[]) {
+        System.out.println(args[0]);
     }
 }
